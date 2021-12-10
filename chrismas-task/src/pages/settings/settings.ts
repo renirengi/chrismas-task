f6e1f7697e0d6ca9d3d0fee655a3f311c4670293
Olga Bogdanova <TomieForever@gmail.com>
2021-12-10 19:34:42 +0300

    feat: add interface

--- chrismas-task/src/pages/settings/settings.ts
+++ chrismas-task/src/pages/settings/settings.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import Page from '../../core/templates/page'
 import { changeVisibility } from '../../utils'
+import data from '../../toys';
 
 
 class SettingsPage extends Page{
@@ -30,8 +32,51 @@ class SettingsPage extends Page{
   makeAllContainer() {
     const toysContainer:HTMLElement= document.createElement('div');
     toysContainer.classList.add('toys-container');
+    this.drawAllToys();
     return toysContainer;
   }
+
+  drawAllToys() {
+    const toys = data.forEach((elem:any) => {
+      const toyContainer:HTMLElement= document.createElement('div');
+      toyContainer.classList.add('toy-container')
+
+      const name:HTMLElement=document.createElement('p');
+      name.textContent=elem.name;
+
+      const count:HTMLElement=document.createElement('p');
+      count.textContent=`Количество: ${elem.count}`;
+
+      const year:HTMLElement=document.createElement('p');
+      year.textContent=`Год покупки: ${elem.year}`;
+
+      const shape:HTMLElement=document.createElement('p');
+      shape.textContent=`Форма игрушки: ${elem.shape}`;
+
+      const color:HTMLElement=document.createElement('p');
+      color.textContent=`Цвет игрушки: ${elem.color}`;
+
+      const size:HTMLElement=document.createElement('p');
+      size.textContent=`Размер игрушки: ${elem.size}`;
+
+      const favorite:HTMLElement=document.createElement('p');
+      favorite.textContent=`Любимая: ${elem.favorite}`;
+
+      toyContainer.append(name);
+      toyContainer.append(count);
+      toyContainer.append(year);
+      toyContainer.append(shape);
+      toyContainer.append(color);
+      toyContainer.append(size);
+      toyContainer.append(favorite);
+
+      console.log(toyContainer);
+    })
+
+    return toys;
+  }
+
+
 
 
 }
